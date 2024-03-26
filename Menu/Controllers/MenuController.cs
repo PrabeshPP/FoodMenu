e4a6336d3d3b232310bfdf98fe4bac6518ba66f4
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-03-25 23:54:20 -0400

    [re-disgned] homepage and model relation

---
+++ Menu/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Menu;
+
+public class MenuController:Controller
+{
+    private readonly ILogger<MenuContext> _logger;
+    private readonly MenuContext menuContext;
+
+    public MenuController(ILogger<MenuContext> _logger, MenuContext menuContext ){
+        this._logger = _logger;
+        this.menuContext = menuContext;
+    }
+
+    public IActionResult Index(){
+        List<Dish> dishes = menuContext.Dishes.ToList();
+        return View("Index",dishes);
+    }
+
+    public IActionResult Detail(int? itemid){
+        Dish? dish = menuContext.Dishes.FirstOrDefault(u=>u.Id == itemid);
+        return View("Detail",dish);
+    }
+}
